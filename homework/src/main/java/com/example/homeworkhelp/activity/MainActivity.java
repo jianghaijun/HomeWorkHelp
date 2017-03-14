9ca7c6423671c503f06c8a5dfb60f09c1216ee83
JiangHaiJun <aini1320666709@foxmail.com>
2017-03-14 17:38:43 +0800

    完善登录界面

--- homework/src/main/java/com/example/homeworkhelp/activity/MainActivity.java
+++ homework/src/main/java/com/example/homeworkhelp/activity/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.homeworkhelp.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.homeworkhelp.R;
+
 /**
  * 主界面
  * @author JiangHaiJun
@@ -15,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
 	}
 }
