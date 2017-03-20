f722e6111e46928d3de1155d262156110d28947e
JiangHaiJun <aini1320666709@foxmail.com>
2017-03-20 16:46:01 +0800

    添加用户列表

--- homework/src/main/java/com/example/homeworkhelp/activity/ChoiceUserActivity.java
+++ homework/src/main/java/com/example/homeworkhelp/activity/ChoiceUserActivity.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.homeworkhelp.R;
 import com.example.homeworkhelp.adapter.ChoiceUserAdapter;
@@ -16,6 +18,7 @@ import com.example.homeworkhelp.bean.UserBean;
 import com.example.homeworkhelp.custom.RecyclerViewDivider;
 import com.example.homeworkhelp.presenter.ChoiceUserPresenter;
 import com.example.homeworkhelp.view.ChoiceUserView;
+import com.scu.miomin.shswiperefresh.core.SHSwipeRefreshLayout;
 
 import org.litepal.crud.DataSupport;
 import org.xutils.view.annotation.Event;
@@ -39,6 +42,8 @@ public class ChoiceUserActivity extends BaseActivity<ChoiceUserView, ChoiceUserP
 
     @ViewInject(R.id.userRclView)
     private RecyclerView userRclView;
+    @ViewInject(R.id.swipeRefreshLayout)
+    private SHSwipeRefreshLayout swipeRefreshLayout;
 
     private static Activity mContext;
     private ChoiceUserAdapter choiceUserAdapter;
@@ -57,6 +62,8 @@ public class ChoiceUserActivity extends BaseActivity<ChoiceUserView, ChoiceUserP
         List<UserBean> userBeans = DataSupport.findAll(UserBean.class);
     
         mContext = this;
+    
+        initSwipeRefreshLayout();
         
         choiceUserAdapter = new ChoiceUserAdapter(this, userBeans);
         userRclView.setLayoutManager(new LinearLayoutManager(this));
@@ -64,6 +71,73 @@ public class ChoiceUserActivity extends BaseActivity<ChoiceUserView, ChoiceUserP
         userRclView.setAdapter(choiceUserAdapter);
     
     }
+
+private void initSwipeRefreshLayout() {
+    swipeRefreshLayout = (SHSwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+    
+    LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
+    final View view = inflater.inflate(R.layout.refresh_view, null);
+    final TextView textView = (TextView) view.findViewById(R.id.title);
+    swipeRefreshLayout.setFooterView(view);
+    swipeRefreshLayout.setOnRefreshListener(new SHSwipeRefreshLayout.SHSOnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            swipeRefreshLayout.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    swipeRefreshLayout.finishRefresh();
+                    Toast.makeText(ChoiceUserActivity.this, "刷新完成", Toast.LENGTH_SHORT).show();
+                }
+            }, 1600);
+        }
+        
+        @Override
+        public void onLoading() {
+            swipeRefreshLayout.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    swipeRefreshLayout.finishLoadmore();
+                    Toast.makeText(ChoiceUserActivity.this, "加载完成", Toast.LENGTH_SHORT).show();
+                }
+            }, 1600);
+        }
+        
+        /**
+         * 监听下拉刷新过程中的状态改变
+         * @param percent 当前下拉距离的百分比（0-1）
+         * @param state 分三种状态{NOT_OVER_TRIGGER_POINT：还未到触发下拉刷新的距离；OVER_TRIGGER_POINT：已经到触发下拉刷新的距离；START：正在下拉刷新}
+         */
+        @Override
+        public void onRefreshPulStateChange(float percent, int state) {
+            switch (state) {
+                case SHSwipeRefreshLayout.NOT_OVER_TRIGGER_POINT:
+                    swipeRefreshLayout.setLoaderViewText("下拉刷新");
+                    break;
+                case SHSwipeRefreshLayout.OVER_TRIGGER_POINT:
+                    swipeRefreshLayout.setLoaderViewText("松开刷新");
+                    break;
+                case SHSwipeRefreshLayout.START:
+                    swipeRefreshLayout.setLoaderViewText("正在刷新");
+                    break;
+            }
+        }
+        
+        @Override
+        public void onLoadmorePullStateChange(float percent, int state) {
+            switch (state) {
+                case SHSwipeRefreshLayout.NOT_OVER_TRIGGER_POINT:
+                    textView.setText("上拉加载");
+                    break;
+                case SHSwipeRefreshLayout.OVER_TRIGGER_POINT:
+                    textView.setText("松开加载");
+                    break;
+                case SHSwipeRefreshLayout.START:
+                    textView.setText("正在加载...");
+                    break;
+            }
+        }
+    });
+}
 
     /**
      * 跳转到登录界面
