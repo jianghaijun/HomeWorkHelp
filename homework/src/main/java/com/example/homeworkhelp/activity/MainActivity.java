55726ca5ca0388c486d38ea171207b180897b62c
JiangHaiJun <aini1320666709@foxmail.com>
2017-03-29 17:39:29 +0800

    添加用户列表

--- homework/src/main/java/com/example/homeworkhelp/activity/MainActivity.java
+++ homework/src/main/java/com/example/homeworkhelp/activity/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.homeworkhelp.activity;
 
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.homeworkhelp.R;
 import com.example.homeworkhelp.utils.SharedPreferencesUtil;
+
+import org.xutils.view.annotation.Event;
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
 
 /**
  * 主界面
@@ -12,11 +20,29 @@ import com.example.homeworkhelp.utils.SharedPreferencesUtil;
  * @time 2017/3/7 14:55
  */
 public class MainActivity extends AppCompatActivity {
+	@ViewInject(R.id.imgBtnLeft)
+	private ImageButton imgBtnLeft;
+	@ViewInject(R.id.drawelayout)
+	private DrawerLayout drawelayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		x.view().inject(this);
+		
+		imgBtnLeft.setImageDrawable(getResources().getDrawable(R.drawable.back_btn));
+		
+	}
+	
+	@Event({ R.id.imgBtnLeft })
+	private void onClick(View view) {
+		switch (view.getId()) {
+			case R.id.imgBtnLeft:
+				drawelayout.openDrawer(GravityCompat.START);
+				break;
+		}
+			
 	}
 
 	@Override
